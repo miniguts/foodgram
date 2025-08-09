d11bbf2563e38e33cfef9fcb3d4661b20faadcd2
pyshechka <tim.vadim.03@gmail.com>
2025-08-09 23:42:08 +0500

    fix: delete default schemas

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -73,7 +73,6 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
